5db212886289d5f3b82aa54f108f8e050dde42d6
Lionel Beato <lbeato@techtalentsouth.com>
2020-02-11 13:59:59 -0500

    array lab

---
+++ script.js
@@ -0,0 +1,17 @@
+let grocery = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
+grocery.unshift('fruit loops');
+grocery[4] = 'fair trade coffee'
+grocery[2] = 'rice'
+grocery[3] = 'beans'
+
+
+console.log(grocery);
+
+//this is an object
+
+let grocery = {
+    name:'orange',
+    amount:3,
+    tasty:true
+}
+
